606146959d73a6dd381ef470fb4e5b812e60bc6a
Jan Habbo Brüning <jan.habbo.bruening@gmail.com>
2025-03-10 15:01:14 +0100

    Fixed php compatibility

--- src/Request.php
+++ src/Request.php
@@ -11,8 +11,7 @@ namespace Frootbox\Http;
 class Request implements Interfaces\RequestInterface { 
     
     protected $server;
-    
-    
+
     /**
      * 
      */
@@ -21,77 +20,135 @@ class Request implements Interfaces\RequestInterface {
         $this->server = $_SERVER;
                 
     }
-    
-        
-    public function getAttribute ( $name, $default = null ) {
-    
-    }
-    
-    public function getAttributes ( ) {
-    
-    }
-    
-    public function getBody (  ) {
-    
-    }
-    
-    public function getQueryParams ( ) {
-    
-    }
-    
-    public function getCookieParams ( ) {
-    
-    }
-    
-    public function getHeader ( $name ) {
-    
-    }
-    
-    public function getHeaderLine ( $name ) {
-    
-    }
-    
-    public function getHeaders ( ) {
-    
-    }
-    
-    
+
     /**
-     * 
+     * @param $name
+     * @param $default
+     * @return void
      */
-    public function getMethod ( ) {
-        
+    public function getAttribute($name, $default = null)
+    {
+    
+    }
+
+    /**
+     * @return array
+     */
+    public function getAttributes(): array
+    {
+    
+    }
+
+    /**
+     * @return \Psr\Http\Message\StreamInterface
+     */
+    public function getBody(): \Psr\Http\Message\StreamInterface
+    {
+    
+    }
+
+    /**
+     * @return array
+     */
+    public function getQueryParams(): array
+    {
+    
+    }
+
+    /**
+     * @return array
+     */
+    public function getCookieParams(): array
+    {
+    
+    }
+
+    /**
+     * @param string $name
+     * @return array|string[]
+     */
+    public function getHeader(string $name): array
+    {
+    
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    public function getHeaderLine(string $name): string
+    {
+    
+    }
+
+    /**
+     * @return array|\string[][]
+     */
+    public function getHeaders(): array
+    {
+    
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod(): string
+    {
         return $this->server['REQUEST_METHOD'];    
     }
-    
-    
+
+    /**
+     * @return void
+     */
     public function getParsedBody ( ) {
     
     }
-    public function getProtocolVersion ( ) {
+
+    /**
+     * @return string
+     */
+    public function getProtocolVersion(): string
+    {
     
     }
-    public function getRequestTarget ( ) {
+
+    /**
+     * @return string
+     */
+    public function getRequestTarget(): string
+    {
     
     }
-    
-    public function getServerParams ( ) {
+
+    /**
+     * @return array
+     */
+    public function getServerParams(): array
+    {
         
     }
-    
-    public function getUploadedFiles ( ) {
+
+    /**
+     * @return array
+     */
+    public function getUploadedFiles(): array
+    {
     
     }
-    public function getUri ( ) {
+
+    /**
+     * @return \Psr\Http\Message\UriInterface
+     */
+    public function getUri(): \Psr\Http\Message\UriInterface
+    {
     
     }
-    
-    
+
     /**
      * 
      */
-    public function getVirtualPath ( ) {
-            
+    public function getVirtualPath()
+    {
         // Get virtual path
         $virtualPath = str_replace(dirname($_SERVER['SCRIPT_NAME']), '', $_SERVER['REQUEST_URI']);
         
@@ -101,69 +158,143 @@ class Request implements Interfaces\RequestInterface {
                        
         return explode('?', $virtualPath)[0];
     }
-    
-    
+
     /**
-     * 
-     * @param unknown $name
+     * @param string $name
+     * @return bool
      */
-    public function hasHeader ( $name ) {
+    public function hasHeader(string $name): bool
+    {
     
     }
-    
-    public function withAddedHeader ( $name, $value ) {
-    
-    }
-    
-    public function withAttribute ( $name, $value ) {
-    
-    }
-    
-    public function withBody ( \Psr\Http\Message\StreamInterface $body ) {
+
+    /**
+     * @param string $name
+     * @param $value
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function withAddedHeader(string $name, $value): \Psr\Http\Message\ResponseInterface
+    {
     
     }
-    
-    public function withCookieParams ( array $cookies ) {
-    
-    }
-    
-    public function withHeader ( $name, $value ) {
-    
-    }
-    
-    public function withMethod ( $method ) {
-    
+
+    /**
+     * @param string $name
+     * @param $value
+     * @return \Psr\Http\Message\ServerRequestInterface
+     */
+    public function withAttribute(string $name, $value): \Psr\Http\Message\ServerRequestInterface
+    {
     
     }
-    public function withParsedBody ( $data ) {
+
+    /**
+     * @param \Psr\Http\Message\StreamInterface $body
+     * @return \Psr\Http\Message\MessageInterface
+     */
+    public function withBody ( \Psr\Http\Message\StreamInterface $body): \Psr\Http\Message\MessageInterface
+    {
     
     }
-    
-    public function withQueryParams ( array $query ) {
-    
-    }
-    public function withProtocolVersion ( $version ) {
-    
-    }
-    
-    public function withRequestTarget ( $requestTarget ) {
+
+    /**
+     * @param array $cookies
+     * @return \Psr\Http\Message\ServerRequestInterface
+     */
+    public function withCookieParams(array $cookies): \Psr\Http\Message\ServerRequestInterface
+    {
     
     }
-    
-    
-    public function withUploadedFiles ( array $uploadedFiles ) {
+
+    /**
+     * @param string $name
+     * @param $value
+     * @return \Psr\Http\Message\ServerRequestInterface
+     */
+    public function withHeader(string $name, $value): \Psr\Http\Message\ServerRequestInterface
+    {
     
     }
-    public function withUri ( \Psr\Http\Message\UriInterface $uri, $preserveHost = false ) {
+
+    /**
+     * @param string $method
+     * @return \Psr\Http\Message\RequestInterface
+     */
+    public function withMethod(string $method): \Psr\Http\Message\RequestInterface
+    {
     
     }
-    public function withoutAttribute ( $name ) {
+
+    /**
+     * @param $data
+     * @return \Psr\Http\Message\ServerRequestInterface
+     */
+    public function withParsedBody($data): \Psr\Http\Message\ServerRequestInterface
+    {
     
     }
+
+    /**
+     * @param array $query
+     * @return \Psr\Http\Message\ServerRequestInterface
+     */
+    public function withQueryParams(array $query): \Psr\Http\Message\ServerRequestInterface
+    {
     
+    }
+
+    /**
+     * @param string $version
+     * @return \Psr\Http\Message\ServerRequestInterface
+     */
+    public function withProtocolVersion(string $version): \Psr\Http\Message\ServerRequestInterface
+    {
     
-    public function withoutHeader ( $name ) {
+    }
+
+    /**
+     * @param $requestTarget
+     * @return \Psr\Http\Message\RequestInterface
+     */
+    public function withRequestTarget($requestTarget): \Psr\Http\Message\RequestInterface
+    {
     
+    }
+
+    /**
+     * @param array $uploadedFiles
+     * @return \Psr\Http\Message\ServerRequestInterface
+     */
+    public function withUploadedFiles(array $uploadedFiles): \Psr\Http\Message\ServerRequestInterface
+    {
+    
+    }
+
+    /**
+     * @param \Psr\Http\Message\UriInterface $uri
+     * @param $preserveHost
+     * @return \Psr\Http\Message\RequestInterface
+     */
+    public function withUri(\Psr\Http\Message\UriInterface $uri, $preserveHost = false): \Psr\Http\Message\RequestInterface
+    {
+    
+    }
+
+    /**
+     * @param string $name
+     * @return \Psr\Http\Message\ServerRequestInterfac
+     */
+    public function withoutAttribute(string $name): \Psr\Http\Message\ServerRequestInterface
+    {
+    
+    }
+
+    /**
+     * @param string $name
+     * @return \Psr\Http\Message\ServerRequestInterface
+     */
+    public function withoutHeader(string $name): \Psr\Http\Message\ServerRequestInterface
+    {
     
     }
 }
